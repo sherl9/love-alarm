a53452c6f4eaa02835ec6006ccc9de0b762dd25e
Sherry Xie <sherry.sy.xie@gmail.com>
2021-09-30 22:43:08 +1000

    Create LoginActivity

---
+++ app/src/main/java/com/comp90018/lovealarm/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.comp90018.lovealarm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText userETLogin, passETLogin;
+    Button loginBtn, registerBtn;
+
+    // Firebase:
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__login);
+
+        userETLogin = findViewById(R.id.enterEmail);
+        passETLogin = findViewById(R.id.enterPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        registerBtn = findViewById(R.id.registerBtn);
+
+        // Firebase Auth:
+        auth = FirebaseAuth.getInstance();
+
+        // Register Button:
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // Login Button:
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email_text = userETLogin.getText().toString();
+                String pass_text = passETLogin.getText().toString();
+
+                // Checking if it is empty
+                if (TextUtils.isEmpty(email_text) || TextUtils.isEmpty(pass_text)){
+                    Toast.makeText( LoginActivity.this, "Please fill the fields", Toast.LENGTH_SHORT).show();
+                } else{
+                    auth.signInWithEmailAndPassword(email_text, pass_text)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(i);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+    }
+}
