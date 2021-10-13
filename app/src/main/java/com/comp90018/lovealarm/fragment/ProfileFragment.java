e521dcfa97b942ac9f4922c5dfdf7edd440ea32c
Princess-Sherry <sherry.sy.xie@gmail.com>
2021-10-14 00:37:41 +1100

    add bottom dialog sheet

--- app/src/main/java/com/comp90018/lovealarm/fragment/ProfileFragment.java
+++ app/src/main/java/com/comp90018/lovealarm/fragment/ProfileFragment.java
@@ -2,13 +2,19 @@ package com.comp90018.lovealarm.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.comp90018.lovealarm.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +22,8 @@ import com.comp90018.lovealarm.R;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+    ImageView avatarImage;
+    Button editButton;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -26,5 +34,27 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        avatarImage = view.findViewById(R.id.profileAvatar);
+        editButton = view.findViewById(R.id.profileEdit);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheetDialog();
+            }
+        });
+    }
+
+    private void showBottomSheetDialog(){
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_profile);
+
+        LinearLayout copy = bottomSheetDialog.findViewById(R.id.cameraLinearLayout);
+        LinearLayout share = bottomSheetDialog.findViewById(R.id.galleryLinearLayout);
+        bottomSheetDialog.show();
     }
 }
