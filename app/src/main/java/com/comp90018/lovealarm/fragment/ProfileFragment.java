9748d7fac679ea01f27d7ede0f9b1649c1b6c540
Princess-Sherry <sherry.sy.xie@gmail.com>
2021-10-14 00:58:46 +1100

    add camera

--- app/src/main/java/com/comp90018/lovealarm/fragment/ProfileFragment.java
+++ app/src/main/java/com/comp90018/lovealarm/fragment/ProfileFragment.java
@@ -1,17 +1,25 @@
 package com.comp90018.lovealarm.fragment;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.comp90018.lovealarm.R;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
@@ -22,6 +30,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
     ImageView avatarImage;
     Button editButton;
 
@@ -52,9 +62,57 @@ public class ProfileFragment extends Fragment {
     private void showBottomSheetDialog(){
         final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
         bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_profile);
+        LinearLayout camera = bottomSheetDialog.findViewById(R.id.cameraLinearLayout);
+        LinearLayout gallery = bottomSheetDialog.findViewById(R.id.galleryLinearLayout);
 
-        LinearLayout copy = bottomSheetDialog.findViewById(R.id.cameraLinearLayout);
-        LinearLayout share = bottomSheetDialog.findViewById(R.id.galleryLinearLayout);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askForCameraPermission();
+                bottomSheetDialog.dismiss();
+            }
+        });
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "Copy is Clicked ", Toast.LENGTH_LONG).show();
+                bottomSheetDialog.dismiss();
+            }
+        });
+
         bottomSheetDialog.show();
+    }
+    private void askForCameraPermission(){
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+            openCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_PERM_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+               openCamera();
+            } else {
+                Toast.makeText(getActivity(), "Camera Permission is required.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCamera(){
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == CAMERA_REQUEST_CODE){
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            avatarImage.setImageBitmap(image);
+        }
     }
 }
