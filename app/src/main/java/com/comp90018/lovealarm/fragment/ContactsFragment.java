3d4899bc83d7fe6a09c4ed44da4c9d270874a400
Yipeng Li <yipengl@student.unimelb.edu.au>
2021-10-19 13:18:31 +0800

    Create ContactsFragment

---
+++ app/src/main/java/com/comp90018/lovealarm/fragment/ContactsFragment.java
@@ -0,0 +1,25 @@
+package com.comp90018.lovealarm.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.comp90018.lovealarm.R;
+
+public class ContactsFragment extends Fragment {
+
+    public ContactsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+}
