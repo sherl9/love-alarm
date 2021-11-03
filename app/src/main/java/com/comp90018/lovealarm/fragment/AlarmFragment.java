d42e8b90b883534806c71e74025acb1a84c952de
ashhher <asher.han.xiao@gmail.com>
2021-11-03 16:28:15 +0800

    add alert map

--- app/src/main/java/com/comp90018/lovealarm/fragment/AlarmFragment.java
+++ app/src/main/java/com/comp90018/lovealarm/fragment/AlarmFragment.java
@@ -1,15 +1,19 @@
 package com.comp90018.lovealarm.fragment;
 
-import static android.content.ContentValues.TAG;
-
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -18,6 +22,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -28,7 +33,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -55,6 +65,7 @@ public class AlarmFragment extends Fragment {
     LottieAnimationView lav_heart_activated;
     LottieAnimationView lav_heart_lover;
     TextView tv_admirersNum;
+    ImageButton btn_map;
 
     private LocationListener locationListener;
     private LocationManager locationManager;
@@ -67,8 +78,8 @@ public class AlarmFragment extends Fragment {
     private List <Coordinate> admirersLocations;
     private List <Coordinate> nearbyAdmirersLocations;
 
-    private final long MIN_TIME = 1000;
-    private final long MIN_DIST = 5;
+    private final long MIN_TIME = 500;
+    private final long MIN_DIST = 1;
     private final long RANGE = 5;
     private static final double EARTH_RADIUS = 6378.137;
 
@@ -78,6 +89,7 @@ public class AlarmFragment extends Fragment {
         @Override
         public void onMapReady(GoogleMap googleMap) {
             mMap = googleMap;
+            mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getContext(), R.raw.map_style));
 
             // update the user location when the location has changed
             locationListener = new LocationListener() {
@@ -136,6 +148,7 @@ public class AlarmFragment extends Fragment {
         lav_heart_activated = view.findViewById(R.id.heart_activated);
         lav_heart_lover = view.findViewById(R.id.heart_lover);
         tv_admirersNum = view.findViewById(R.id.admirers_num);
+        btn_map = view.findViewById(R.id.btn_map);
 
         // ask for location permissions
         ActivityCompat.requestPermissions(requireActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
@@ -239,6 +252,14 @@ public class AlarmFragment extends Fragment {
 
             }
         });
+
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapFragment.getView().setVisibility(View.VISIBLE);
+                btn_map.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
     // update nearby admirers
@@ -251,13 +272,25 @@ public class AlarmFragment extends Fragment {
         for (Coordinate coordinate : admirersLocations) {
             LatLng latLng = new LatLng(coordinate.getLatitude(), coordinate.getLongitude());
             if(coordinate.getUserId().equals(userId)) {
-                mMap.addMarker(new MarkerOptions().position(latLng).title(coordinate.getUserId()));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                mMap.addCircle(new CircleOptions()
+                        .center(latLng)
+                        .radius(500)
+                        .strokeWidth(3f)
+                        .strokeColor(Color.rgb(241, 147, 156))
+                        .fillColor(Color.argb(50, 241, 147, 156)));
+                mMap.addMarker(new MarkerOptions().position(latLng).title("You")
+                        .icon(BitmapFromVector(getActivity(), R.drawable.ic_marker_my)));
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
             } else if(getDistance(userLocation.getLongitude(), userLocation.getLatitude(), coordinate.getLongitude(), coordinate.getLatitude()) < RANGE){
                 nearbyAdmirersLocations.add(coordinate);
-                mMap.addMarker(new MarkerOptions().position(latLng).title(coordinate.getUserId()));
                 if (coordinate.getUserId().equals(lover.getUserId())) {
                     isLoverNear = true;
+                    mMap.addMarker(new MarkerOptions().position(latLng)
+                            .icon(BitmapFromVector(getActivity(), R.drawable.ic_marker_lover)));
+                } else {
+                    mMap.addMarker(new MarkerOptions().position(latLng)
+                            .icon(BitmapFromVector(getActivity(), R.drawable.ic_marker_admirer)));
                 }
             }
         }
@@ -284,6 +317,8 @@ public class AlarmFragment extends Fragment {
         }
     }
 
+
+
     // calculate the distance between two nodes based on their coodinates
     public static double getDistance(double longitude1, double latitude1, double longitude2, double latitude2) {
         double lat1 = Math.toRadians(latitude1);
@@ -296,6 +331,28 @@ public class AlarmFragment extends Fragment {
         s =  s * EARTH_RADIUS;
         System.out.println("Distance: " + s);
         return s;
+    }
+
+    private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
+        // below line is use to generate a drawable.
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        // below line is use to add bitmap in our canvas.
+        Canvas canvas = new Canvas(bitmap);
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas);
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
 }
